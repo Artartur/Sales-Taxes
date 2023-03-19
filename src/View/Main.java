a2e93e1780875ba9aa307584bf2ea2c22fc37046
artartur <arturgfs@hotmail.com>
2023-03-19 12:56:41 -0300

    Deleted some classes and add new streams in main class

--- src/View/Main.java
+++ src/View/Main.java
@@ -1,8 +1,6 @@
 package View;
 
-import Controller.ProductOptions.BuyBook;
-import Controller.ProductOptions.BuyFood;
-import Controller.ProductOptions.BuyMedical;
+import Controller.ProductOptions.ExemptProducts;
 import Controller.ProductOptions.BuyOther;
 import Model.Product;
 
@@ -17,10 +15,8 @@ public class Main {
         Scanner inputScanner = new Scanner(System.in);
         ArrayList<Product> products = new ArrayList<Product>();
 
-        BuyFood foodInstance = new BuyFood();
-        BuyBook bookInstance = new BuyBook();
-        BuyMedical medicalInstance = new BuyMedical();
-        BuyOther otherInstance = new BuyOther();
+        ExemptProducts exemptProductsInstances = new ExemptProducts();
+        BuyOther otherInstances = new BuyOther();
 
         System.out.println("Welcome to RayMarket!");
         System.out.println("How can I help you?");
@@ -28,7 +24,7 @@ public class Main {
         int a = inputScanner.nextInt();
         int[] array = new int[a];
         System.out.println("Enter the number representing the product you would like to buy: ");
-        System.out.println(" 1 - Food \n 2 - Book \n 3 - Medical products \n 4 - Other \n 0 - Cancel");
+        System.out.println(" 1 - Food, Book or Medical products \n 2 - Other \n 0 - Cancel");
 
         int option = inputScanner.nextInt();
 
@@ -36,19 +32,11 @@ public class Main {
 
             switch (option) {
                 case 1:
-                    Product food = foodInstance.buyFood(inputScanner);
-                    products.add(food);
+                    Product exemptProducts = exemptProductsInstances.BuyExemptProducts(inputScanner);
+                    products.add(exemptProducts);
                     break;
                 case 2:
-                    Product book = bookInstance.buyBook(inputScanner);
-                    products.add(book);
-                    break;
-                case 3:
-                    Product medical = medicalInstance.buyMedical(inputScanner);
-                    products.add(medical);
-                    break;
-                case 4:
-                    Product other = otherInstance.buyOther(inputScanner);
+                    Product other = otherInstances.buyOther(inputScanner);
                     products.add(other);
                     break;
                 default:
@@ -56,52 +44,41 @@ public class Main {
                     break;
             }
             System.out.println("Enter the number representing the product you would like to buy: ");
-            System.out.println(" 1 - Food \n 2 - Book \n 3 - Medical products \n 4 - Other \n 0 - Cancel");
+            System.out.println(" 1 - Food, Book or Medical products \n 2 - Other \n 0 - Cancel");
             option = inputScanner.nextInt();
         }
 
 
         double price= 0;
         double priceTaxed = 0;
-        double priceImportTaxed = 0;
-        double priceImportPlusTaxed = 0;
-                    String prod = products.stream().map(product -> String.format("%s", product.getProductName())).collect(Collectors.joining(", \n"));
-                    System.out.println(prod);
-                    price = products.stream().mapToDouble(Product::getPrice).reduce(0, Double::sum);
-                    priceTaxed = products.stream().mapToDouble(Product::importTax).reduce(0, Double::sum);
-                    priceImportPlusTaxed = products.stream().mapToDouble(Product::importTaxPlusTax).reduce(0, Double::sum);
-                    System.out.println("Sales Taxes: " + priceTaxed);
-                    System.out.println("Total: "+ (priceTaxed + price));
 
-                    price = products.stream().mapToDouble(Product::getPrice).reduce(0, Double::sum);
-                    priceImportTaxed = products.stream().mapToDouble(Product::importTaxPlusTax).reduce(0, Double::sum);
-                    double total = priceTaxed + priceImportTaxed;
-                    System.out.println("Sales Taxes: " + total);
-                    System.out.println("Total: "+ (priceTaxed + price));
+        String prod = products.stream().map(product -> String.format("%s", product.getProductName())).collect(Collectors.joining(", \n"));
+        System.out.println(prod);
 
-            }
+        //ExemptProducts
+
+        price = products.stream().mapToDouble(Product::getPrice).reduce(0, Double::sum);
+        priceTaxed = products.stream().mapToDouble(Product::importTax).reduce(0, Double::sum);
+        System.out.println("price: "+ price);
+        System.out.println("Sales Taxes: " + (priceTaxed-price));
+        System.out.println("Total: "+ (priceTaxed));
+
+
+        // Others
+
+        price = products.stream().mapToDouble(Product::getPrice).reduce(0, Double::sum);
+        priceTaxed = products.stream().mapToDouble(Product::importTaxPlusTax).reduce(0, Double::sum);
+
+        System.out.println("Sales Taxes: " + (priceTaxed-price));
+        System.out.println("Total: "+ (priceTaxed));
+
+        price = products.stream().mapToDouble(Product::getPrice).reduce(0, Double::sum);
+        priceTaxed = products.stream().mapToDouble(Product::tax).reduce(0, Double::sum);
+
+        System.out.println("Sales Taxes: " + (priceTaxed-price));
+        System.out.println("Total: "+ (priceTaxed));
 
 
     }
+    }
 
-//    double total = 0;
-//        double amountTaxes = 0;
-//        for (Product product : products){
-//            switch (product.isImported){
-//                case 1:
-//                    System.out.printf("%d - imported %s: %.2f\n", product.getAmount(), product.getProductName(), product.testImport());
-//                    amountTaxes += product.amountTaxes();
-//                    total += product.testImport() * product.getAmount();
-//                    break;
-//                case 2:
-//                    System.out.printf("%d - %s: %.2f\n", product.getAmount(), product.getProductName(), product.getPrice());
-//                    total += product.getPrice() * product.getAmount();
-//                    break;
-//                default:
-//                    System.out.println("Enter a valid number");
-//                    break;
-//            }
-//        }
-//        System.out.printf("Sales Taxes: %.2f\n", amountTaxes);
-//        System.out.printf("Total: %.2f", total);
-//        }
